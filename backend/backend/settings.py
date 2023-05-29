c212e67c8d4c12ee694fece922787ee2e1708b85
Никита Петров <nikumtech@yandex.ru>
2023-05-29 18:18:50 +0300

    Flake8 fixings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,6 +116,4 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
